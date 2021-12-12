47ea652f133aba0f22f45d1fe98e202e8543cdb8
Wellington Vieira <wellington.vieira@fatec.sp.gov.br>
2021-12-11 21:51:00 -0300

    teste

---
+++ demo/src/main/java/br/com/community/community99jobs/Community99jobsApplication.java
@@ -0,0 +1,13 @@
+package br.com.community.community99jobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Community99jobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Community99jobsApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/br/com/community/community99jobs/Community99jobsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.community.community99jobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Community99jobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
